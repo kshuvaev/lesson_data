f165a990f9e414772249c7ceca30a1b8f8b91781
kshuvaev <chamga2010@gmail.com>
2024-11-06 18:01:26 +0500

    Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as f_
+import true_math as t_
+
+result1 = f_.divide(69, 3)
+result2 = f_.divide(3, 0)
+result3 = t_.divide(49, 7)
+result4 = t_.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
